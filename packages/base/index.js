c3b9f514738a7d075f91f704b8b26fbfc039ed4a
Steven Weingärtner <pd@frozen-bits.de>
2017-05-15 13:10:03 +0200

    Excluded jest rules into own package
    Not every JS project uses jest for testing.

--- packages/base/index.js
+++ packages/base/index.js
@@ -6,15 +6,6 @@ module.exports = {
     rules: {
         indent: ['error', 4, {
             SwitchCase: 1,
-        }],
-        'jest/no-disabled-tests': 'warn',
-        'jest/no-focused-tests': 'error',
-        'jest/no-identical-title': 'error',
-    },
-    plugins: [
-        'jest',
-    ],
-    env: {
-        'jest/globals': true,
-    },
+        }]
+    }
 };
---
+++ packages/jest/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+    },
+    plugins: [
+        'jest',
+    ],
+    env: {
+        'jest/globals': true,
+    },
+}
