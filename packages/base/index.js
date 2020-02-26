42822aca9ff9a619604c2b921cf9fb8ff58b1f81
Steven Weingärtner <sweingaertner@brainbits.net>
2020-02-26 13:43:38 +0100

    Add recommended typescript rules

--- packages/base/index.js
+++ packages/base/index.js
@@ -6,7 +6,7 @@ module.exports = {
     extends: [
         'eslint-config-airbnb-base',
         'eslint-config-airbnb-base/rules/strict',
-    ].map(require.resolve),
+    ],
     plugins: [
         'babel',
     ],
